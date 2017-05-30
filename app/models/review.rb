4ff6a86aed2f0fbb363b526237c4ffe1e99592f3
Tim Almamedov <tim.almamedov@gmail.com>
2017-05-30 13:25:57 +0200

    Seed file done, Review validation fixed

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :flat
   validates :content, presence: true
-  validates :rating, inclusion: { in (1..5)}
+  validates :rating, inclusion: { in: (1..5) }
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,81 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
+puts "Seeding database"
+
+user_seed = [
+  {
   first_name: "Tim",
-  last_name: "Almamedov"
+  last_name: "Almamedov",
+  email: "tim.almamedov@gmail.com",
+  birthdate: Date.new(1993,10,19),
+  password: "password",
+  password_confirmation: "password",
+  gender: 'Male',
+  address: 'tims address',
+  phone_number: '+43 660 479 42 52',
+  about: "my info dlfgjh dljfsdjf sodijfsoid sdofjisiodf"
+  },
 
+  {
+  first_name: "John",
+  last_name: "Dawson",
+  email: "john.dawson@gmail.com",
+  birthdate: Date.new(1983,4,9),
+  password: "secret",
+  password_confirmation: "secret",
+  gender: 'Male',
+  address: "John's address",
+  phone_number: '+32 490 630 94 11',
+  about: "my info dlfgjh dljfsdjf sodijfsoid sdofjisiodf"
+  }
+]
 
+flat_seed = [
+  {
+  description: "Thats a long description",
+  short_description: "A lovely home",
+  user_id: 1,
+  room_count: 5,
+  property_type: "House",
+  price: 100,
+  country: "Austria",
+  city: "Vienna",
+  street: "some street 25/3",
+  zipcode: "1220"
+  },
 
+  {
+  description: "Thats a long description",
+  short_description: "Nice studio",
+  user_id: 2,
+  room_count: 1,
+  property_type: "Studio",
+  price: 40,
+  country: "Belgium",
+  city: "Brussels",
+  street: "some street 2",
+  zipcode: "1008"
+  }
+]
 
+User.create!(user_seed)
+Flat.create!(flat_seed)
+
+Booking.create!(
+  start_date: Date.new(2017,7,20),
+  end_date: Date.new(2017,7,29),
+  status: "Pending",
+  user_id: 1,
+  flat_id: 2
   )
+
+Review.create!(
+  content: "A very nice apartment",
+  rating: 4,
+  user_id: 2,
+  flat_id: 1
+  )
+
+
+puts "Seeding done"
