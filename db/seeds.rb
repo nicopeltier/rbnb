9cd71b02d7f9e7bffeed3952de966dcf151b08d4
Nicolas Peltier <nicolas.peltier@gmail.com>
2017-05-31 16:30:39 +0200

    seeds 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 puts "Seeding database"
 
 user_seed = [
