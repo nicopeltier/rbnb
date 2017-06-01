b35d367a287a3baa5b196f3ea5bc01e07dfb0508
Tim Almamedov <tim.almamedov@gmail.com>
2017-06-01 16:56:16 +0200

    changed my properties tab

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,11 +6,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   process convert: 'jpg'
 
   version :standard do
-    cloudinary_transformation width: 450, height: 200, crop: :thumb
+    cloudinary_transformation width: 450, height: 200, crop: :thumb, :radius=>"5:5:0:0"
   end
 
   version :big do
-    cloudinary_transformation width: 650, height: 270, crop: :thumb
+    cloudinary_transformation width: 650, height: 270, crop: :thumb, :radius=>"5:5:0:0"
   end
 
   version :show do
