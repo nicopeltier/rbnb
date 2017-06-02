4dd99d9721a8497f9d94bf102945858be4722355
Nicolas Peltier <nicolas.peltier@gmail.com>
2017-06-02 18:18:25 +0200

    email

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -6,12 +6,12 @@ class BookingMailer < ApplicationMailer
   #   en.booking_mailer.newbooking.subject
   #
   def newbooking(booking)
+
     @greeting = "Hello"
     @booking = booking
     @user = User.find(@booking.user_id)
     @flat = Flat.find(@booking.flat_id)
 
-    mail to: "to@example.org"
 
     mail(to: @user.email, subject: 'You have a new booking')
     # This will render a view in `app/views/user_mailer`!
